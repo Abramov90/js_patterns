59646b188811af9db04ffa1eddc91f4b06faa39d
Viktor Abramov <abramov90@gmail.com>
2016-04-17 00:08:16 +0300

    Added one more implementation of Module pattern

--- Module.js
+++ Module.js
@@ -8,17 +8,36 @@ var ModuleExample = (function() {
 		console.log('You entered ' + prop);
 	};
 
-	return {
-
-		somePublicValue: "value",
-
-		getCount: function() {
-			return _privateCounter;
-		},
-
-		somePublicMethod: function(prop) {
-			_somePrivateMethod(prop);
-		}
+	this.someMethod = function _somePrivateMethod(prop) {
+		_somePrivateMethod(prop);
 	};
-	
+
 })();
+
+/* Another Implementation */
+
+(function(window) {
+
+	function ModuleExample() {
+
+		var _privateCount = 0;
+
+		var _privateIncreaseCount = function() {
+			_privateCount++;
+		};
+
+		var _privateDecreaseCount = function() {
+			_privateCount++;
+		};
+
+		this.publicMethod = _privateIncreaseCount;
+
+		this.decreaseCount = _privateDecreaseCount;
+
+		this.getCount = function() { return _privateCount; };
+	};
+
+	window.ModuleExample = ModuleExample;
+
+})(window);
+
